584e9cd5bf744ab4d74bd637cc69d47d6e4bed67
Lin Wen-Han <49276651+jerry4911@users.noreply.github.com>
2021-11-15 23:57:41 +0800

    City Section
    1. 新增熱門城市區塊
    2. 功能未實作
    3. 切版還沒處理好

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,11 +1,15 @@
 import React from "react";
-
-
+import map from "../assets/images/map.svg"
 
 function CityCard(props) {
   const city_card = `city-card city-card-${props.size}`
   return (
-    <img className={city_card} src={props.city} alt={props.name}></img>
+    <div className={city_card}>
+      <img src={props.city} alt={props.name}></img>
+      <div className="city-card-mask"></div>
+      <embed className="city-icon" src={map} alt="map"></embed>
+      <p>{props.name}</p>
+    </div>
   );
 }
 
--- src/components/CitySection.js
+++ src/components/CitySection.js
@@ -1,11 +1,32 @@
 import React from "react";
+import icon from "../assets/images/triangle.svg"
 import taipei_city from "../assets/images/city/台北市.png";
+import new_taipei_city from "../assets/images/city/新北市.png";
+import momo_city from "../assets/images/city/桃園市.png";
+import hsinchu_city from "../assets/images/city/新竹市.png";
+import taichung from "../assets/images/city/台中.png";
+import Nantou from "../assets/images/city/南投.png";
+import Chiayi from "../assets/images/city/嘉義.png";
 import CityCard from "./CityCard";
 
 function CitySection(props) {
   return (
     <div className="city-section">
+      <div className="title">
+        <embed className="title-icon" src={icon} alt="map"></embed>
+        <p>熱門城市</p>
+      </div>
       <CityCard city={taipei_city} name="台北市" size="big"></CityCard>
+      <div className="two-city-card">
+        <CityCard city={new_taipei_city} name="新北市" size="small"></CityCard>
+        <CityCard city={momo_city} name="桃園市" size="small"></CityCard>
+      </div>
+      <CityCard city={hsinchu_city} name="新竹市" size="big"></CityCard>
+      <div className="two-city-card">
+        <CityCard city={taichung} name="台中" size="small"></CityCard>
+        <CityCard city={Nantou} name="南投" size="small"></CityCard>
+      </div>
+      <CityCard city={Chiayi} name="嘉義" size="big"></CityCard>
     </div>
   );
 }
